99ecb9bcfcc2ba3759b6d25b54e1f040e5ea21f8
enzinho <enzo.paxpayne@gmail.com>
2023-02-07 05:36:00 -0300

    finalizando

--- index.rb
+++ index.rb
@@ -11,7 +11,13 @@ sleep 5
 
 cliente = clientes[id_cliente]
 
-puts("Nome: #{cliente[:nome]}")
-puts("Akuma no mi: #{cliente[:akuma_no_mi]}")
-puts("data de cadastro: #{cliente[:data_de_cadastro]}")
+if cliente != nil?
+  puts("Nome: #{cliente[:nome]}")
+  puts("Akuma no mi: #{cliente[:akuma_no_mi]}")
+  puts("data de cadastro: #{cliente[:data_de_cadastro]}")
+  puts
+  puts "programa finalizado! 🤪"
+else
+  puts("Usuario não encontrado")
+end
 
